9639870aa0e8bcfd9b033b4024b49f070880247a
sahilpatel18 <sahil.41801@gmail.com>
2020-10-04 23:37:24 -0400

    fetching trivia questions and specific difficulty

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React, { useState } from "react";
-import QuestionCard from "./components/QuestionCard";
+import { fetchQuizQuestions } from "./API";
+//import QuestionCard from "./components/QuestionCard";
+
+import {Difficulty} from './API'
+
+const TOTAL_QUESTIONS = 10;
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+
   const startTrivia = async () => {};
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
@@ -14,7 +28,14 @@ const App = () => {
       <button className='start' onClick={startTrivia}></button>
       <p className='score'>Score:</p>
       <p>Loading Questions...</p>
-      <QuestionCard />
+      {/* <QuestionCard
+        questionNumber={number + 1}
+        answers={questions[number].answers}
+        question={questions[number].question}
+        callback={checkAnswer}
+        totalQuestions={TOTAL_QUESTIONS}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+      /> */}
       <button className='next' onClick={nextQuestion}>
         Next Question
       </button>
